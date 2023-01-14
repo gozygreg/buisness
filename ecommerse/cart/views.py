c3f2655041bf55cfdb6fb509468bcccebc49a1af
gozygreg <goziennabuife@yahoo.com>
2023-01-14 00:09:47 +0000

    create cart app and configiure basic urls and views for our cart

---
+++ ecommerse/cart/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def cart_summary(request):
+    Pass
+
+
+def cart_add(request):
+    Pass
+
+
+def cart_delete(request):
+    Pass
+
+
+def cart_update(request):
+    Pass
+
