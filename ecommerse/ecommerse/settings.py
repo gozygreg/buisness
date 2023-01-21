b9dc8c0ed4031483df598d95f56098adf9a47741
gozygreg <goziennabuife@yahoo.com>
2023-01-21 05:44:14 +0000

    Styling the email verification templates

--- ecommerse/ecommerse/settings.py
+++ ecommerse/ecommerse/settings.py
@@ -140,4 +140,12 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email configuration settings:
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'gregmeditechinc@gmail.com'  # Enter your Gmail address
+EMAIL_HOST_PASSWORD = 'bfstgqbcpnlnqxou'  # Enter your app password
