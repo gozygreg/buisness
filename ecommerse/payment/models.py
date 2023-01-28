e76c1c3252621043c511d05cef17f95410734062
gozygreg <goziennabuife@yahoo.com>
2023-01-28 17:52:31 +0000

    Build shipping address model

--- ecommerse/payment/models.py
+++ ecommerse/payment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShippingAddress(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200, null=True, blank=True)
+    postcode = models.CharField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Shipping Address'
+
+    def __str__(self):
+        return 'Shipping Address - ' + str(self.id)
