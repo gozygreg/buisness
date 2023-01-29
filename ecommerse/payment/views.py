e748ce1099ac3e6c214c0701c2040047f0b47a35
gozygreg <goziennabuife@yahoo.com>
2023-01-29 19:31:16 +0000

    Build the checkout template

--- ecommerse/payment/views.py
+++ ecommerse/payment/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def checkout(request):
+    return render(request, 'payment/checkout.html')
+
+
 def payment_success(request):
     return render(request, 'payment/payment-success.html')
 
