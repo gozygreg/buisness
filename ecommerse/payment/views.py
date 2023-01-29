8a8ff606861d53e5c6a46b2c1720e74f066e5fdb
gozygreg <goziennabuife@yahoo.com>
2023-01-29 19:57:30 +0000

    Build checkout functionality

--- ecommerse/payment/views.py
+++ ecommerse/payment/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from . models import ShippingAddress
 
 # Create your views here.
 
 
 def checkout(request):
-    return render(request, 'payment/checkout.html')
+    # users with accounts -- pre-fill the form
+    if request.user.is_authenticated:
+        try:
+            # Authenticated users with shipping information
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+            context = {'shipping': shipping_address}
+            return render(request, 'payment/checkout.html', context=context)
+        except:
+            # Authenticated users without shipping information
+            return render(request, 'payment/checkout.html')
+    else:
+        # Guest user
+        return render(request, 'payment/checkout.html')
 
 
 def payment_success(request):
