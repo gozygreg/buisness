eca3fd3836fce3c20cad2fb27d2eb241c040d15d
gozygreg <goziennabuife@yahoo.com>
2023-01-29 21:17:38 +0000

    Add AJAX redirection and clear the shopping cart functionality

--- ecommerse/payment/views.py
+++ ecommerse/payment/views.py
@@ -66,11 +66,11 @@ def complete_order(request):
         return response
 
 
-
-
-
-
 def payment_success(request):
+    # Clear shopping cart
+    for key in list(request.session.keys()):
+        if key == 'session_key':
+            del request.session[key]
     return render(request, 'payment/payment-success.html')
 
 
